26b5c70844c2b4aca7d4d1a4c5c77768ce087e23
Tanimismat <tanim.ismat@gmail.com>
2022-04-28 20:43:49 +0600

    sending data from client side

---
+++ src/Components/AddUser.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const AddUser = () => {
+    const handleAddUser = event => {
+        event.preventDefault();
+        console.log('clicked')
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const user = { name, email };
+        console.log(user);
+        //send data to the server
+        fetch('http://localhost:5000/user', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(user),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+    }
+    return (
+        <div>
+            <h2>This is add user</h2>
+            <form onSubmit={handleAddUser}>
+                <input type="text" name='name' placeholder='Name' required />
+                <br />
+                <input type="email" name='email' placeholder='Email' required />
+                <br />
+                <input type="submit" value="Add User" />
+            </form>
+        </div>
+    );
+};
+
+export default AddUser;
